f18631b9dffb87741d0f3e8622a8769368de6aa2
HassanElshazlyEida <HassanElshazlyEida@users.noreply.github.com>
2022-11-07 15:45:11 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,17 +4,18 @@ namespace HassanElshazlyEida\UnitConversions;
 
 class Weight
 {
-
-    public static function fromKg(float $kg):self{
+    public static function fromKg(float $kg): self
+    {
         return new static($kg);
     }
 
     public function __construct(float $kg)
     {
-        $this->kg=$kg;
+        $this->kg = $kg;
     }
-    
-    public function toLbs():float {
+
+    public function toLbs(): float
+    {
         return $this->kg * 2.204623;
     }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,9 +3,7 @@
 use HassanElshazlyEida\UnitConversions\Weight;
 
 it('can convert kg to lbs', function () {
-
-    $lbs=Weight::fromKg(100)->toLbs();
+    $lbs = Weight::fromKg(100)->toLbs();
 
     $this->assertEquals(220.4623, number_format($lbs, 4, '.', ''));
-
 });
