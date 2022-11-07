ee08241b750b039b71c4449f31fb420a8d0c0c25
Hassan Elshazly Eida <hassanaboeata@gmail.com>
2022-11-07 22:02:40 +0200

    👌 IMPROVE: Change PHP version

--- src/Weight.php
+++ src/Weight.php
@@ -4,7 +4,10 @@ namespace HassanElshazlyEida\UnitConversions;
 
 class Weight
 {
-
+    /**
+     * @var float
+     */
+    private $kg;
     public static function fromKg(float $kg):self{
         return new static($kg);
     }
@@ -13,7 +16,7 @@ class Weight
     {
         $this->kg=$kg;
     }
-    
+
     public function toLbs():float {
         return $this->kg * 2.204623;
     }
